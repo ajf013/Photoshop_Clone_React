37f2a17762708c8f7a65a7e3042ab3eb1a145372
ajf013 <jeni13franc@gmail.com>
2021-04-03 22:06:55 +0530

    Added Choose File mode

--- src/App.js
+++ src/App.js
@@ -1,119 +1,147 @@
-import React, { useState } from 'react';
-import './App.css';
-import Slider from './Slider'
-import SidebarItem from './SidebarItem'
+import React, { useState } from "react";
+import "./App.css";
+import SidebarItem from "./components/SidebarItem";
+import Slider from "./components/Slider";
+import * as htmlToImage from "html-to-image";
+import * as download from "downloadjs";
 
 const DEFAULT_OPTIONS = [
   {
-    name: 'Brightness',
-    property: 'brightness',
+    name: "Brightness",
+    property: "brightness",
     value: 100,
     range: {
       min: 0,
-      max: 200
+      max: 200,
     },
-    unit: '%'
+    unit: "%",
   },
   {
-    name: 'Contrast',
-    property: 'contrast',
+    name: "Contrast",
+    property: "contrast",
     value: 100,
     range: {
       min: 0,
-      max: 200
+      max: 200,
     },
-    unit: '%'
+    unit: "%",
   },
   {
-    name: 'Saturation',
-    property: 'saturate',
+    name: "Saturation",
+    property: "saturate",
     value: 100,
     range: {
       min: 0,
-      max: 200
+      max: 200,
     },
-    unit: '%'
+    unit: "%",
   },
   {
-    name: 'Grayscale',
-    property: 'grayscale',
+    name: "Grayscale",
+    property: "grayscale",
     value: 0,
     range: {
       min: 0,
-      max: 100
+      max: 100,
     },
-    unit: '%'
+    unit: "%",
   },
   {
-    name: 'Sepia',
-    property: 'sepia',
+    name: "Sepia",
+    property: "sepia",
     value: 0,
     range: {
       min: 0,
-      max: 100
+      max: 100,
     },
-    unit: '%'
+    unit: "%",
   },
   {
-    name: 'Hue Rotate',
-    property: 'hue-rotate',
+    name: "Hue Rotate",
+    property: "hue-rotate",
     value: 0,
     range: {
       min: 0,
-      max: 360
+      max: 360,
     },
-    unit: 'deg'
+    unit: "deg",
   },
   {
-    name: 'Blur',
-    property: 'blur',
+    name: "Blur",
+    property: "blur",
     value: 0,
     range: {
       min: 0,
-      max: 20
+      max: 20,
     },
-    unit: 'px'
-  }
-]
+    unit: "px",
+  },
+];
 
-function App() {
-  const [selectedOptionIndex, setSelectedOptionIndex] = useState(0)
-  const [options, setOptions] = useState(DEFAULT_OPTIONS)
-  const selectedOption = options[selectedOptionIndex]
+const App = () => {
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const [options, setOptions] = useState(DEFAULT_OPTIONS);
+  const [image, setImage] = useState(null);
 
-  function handleSliderChange({ target }) {
-    setOptions(prevOptions => {
+  const selectedOption = options[selectedIndex];
+
+  const handleSliderChange = ({ target }) => {
+    setOptions((prevOptions) => {
       return prevOptions.map((option, index) => {
-        if (index !== selectedOptionIndex) return option
-        return { ...option, value: target.value }
-      })
-    })
-  }
+        if (index !== selectedIndex) return option;
+        return { ...option, value: target.value };
+      });
+    });
+  };
 
-  function getImageStyle() {
-    const filters = options.map(option => {
-      return `${option.property}(${option.value}${option.unit})`
-    })
+  const applyFilters = () => {
+    const filters = options.map((option) => {
+      return `${option.property}(${option.value}${option.unit})`;
+    });
 
-    return { filter: filters.join(' ') }
-  }
+    return {
+      filter: filters.join(" "),
+      backgroundImage: `url(${image})`,
+    };
+  };
 
-  console.log(getImageStyle())
+  const downloadResume = () => {
+    htmlToImage
+      .toPng(document.getElementById("image"))
+      .then(function (dataUrl) {
+        download(dataUrl, `${Date.now()}.png`);
+      });
+  };
+
+  const handleImage = (event) => {
+    const objectUrl = URL.createObjectURL(event.target.files[0]);
+    setImage(objectUrl);
+  };
 
   return (
     <div className="container">
-      <div className="main-image" style={getImageStyle()} />
+      {image ? (
+        <div className="main-image" style={applyFilters()} id="image" />
+      ) : (
+        <div className="upload-image">
+          <h1>Photoshop Clone React</h1>
+          <input type="file" accept="image/*" onChange={handleImage} />
+        </div>
+      )}
       <div className="sidebar">
         {options.map((option, index) => {
           return (
             <SidebarItem
               key={index}
               name={option.name}
-              active={index === selectedOptionIndex}
-              handleClick={() => setSelectedOptionIndex(index)}
+              active={index === selectedIndex}
+              handleClick={() => setSelectedIndex(index)}
             />
-          )
+          );
         })}
+        <button onClick={downloadResume} className="download">
+          Download
+        </button>
       </div>
       <Slider
         min={selectedOption.range.min}
@@ -122,7 +150,7 @@ function App() {
         handleChange={handleSliderChange}
       />
     </div>
-  )
-}
+  );
+};
 
 export default App;
